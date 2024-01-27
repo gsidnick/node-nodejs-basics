f891503d2d7c1e0ae3a6c29373c615baf17d16ed
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-27 12:02:26 +0300

    feat: implement cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argv = process.argv.slice(2);
+    const result = argv
+        .reduce(
+            (values, key, index) =>
+                (index % 2 == 0 ? values.push([key.replace(/^--/, "")]) : values[values.length - 1].push(key)) &&
+                values,
+            [],
+        )
+        .map((value) => value.join(" is "))
+        .join(", ");
+    console.log(result);
 };
 
 parseArgs();
