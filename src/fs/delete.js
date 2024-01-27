c33f6be20a07bcea792a7b3beb4b654cd40f14c4
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-27 08:48:13 +0300

    feat: implement fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import { access, constants, unlink } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filename = join(__dirname, "files", "fileToRemove.txt");
+
+const isExist = async (pathname) => {
+    try {
+        await access(pathname, constants.F_OK);
+        return true;
+    } catch (error) {
+        if (error.code !== "ENOENT") console.error(error);
+        return false;
+    }
+};
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const isFileToRemoveExist = await isExist(filename);
+        if (!isFileToRemoveExist) throw new Error("FS operation failed");
+        unlink(filename);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await remove();
