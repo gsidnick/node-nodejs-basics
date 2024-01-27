f6277db98a2d4a12213e2ec8bcb3d8ac0a52acc4
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-27 09:51:47 +0300

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,30 @@
+import { access, constants, open } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filename = join(__dirname, "files", "fileToRead.txt");
+
+const isExist = async (pathname) => {
+    try {
+        await access(pathname, constants.F_OK);
+        return true;
+    } catch (error) {
+        if (error.code !== "ENOENT") console.error(error);
+        return false;
+    }
+};
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const isFileExist = await isExist(filename);
+        if (!isFileExist) throw new Error("FS operation failed");
+        const fd = await open(filename, "r");
+        const data = await fd.readFile({ encoding: "utf-8" });
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await read();
