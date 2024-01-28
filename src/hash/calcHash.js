b7cf83e69277637dfa86bdc0cc0b65d3980158d5
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-28 11:05:11 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { dirname, join } from "node:path";
+import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filename = join(__dirname, "files", "fileToCalculateHashFor.txt");
+    const hash = createHash("sha256");
+    const input = createReadStream(filename);
+
+    input.on("readable", () => {
+        const data = input.read();
+        if (data) {
+            hash.update(data);
+        } else {
+            stdout.write(`${hash.digest("hex")}`);
+        }
+    });
 };
 
 await calculateHash();
