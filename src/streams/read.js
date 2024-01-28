6e84992de9f0c8704e8af8e329e725d066c1e070
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-28 14:27:56 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "node:fs";
+import { dirname, join } from "node:path";
+import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const filename = join(__dirname, "files", "fileToRead.txt");
+    const input = createReadStream(filename, { encoding: "utf-8" });
+    let data = "";
+
+    input.on("data", (chunk) => (data += chunk));
+    input.on("end", () => stdout.write(data));
+    input.on("error", (error) => stdout.write(error.message));
 };
 
 await read();
