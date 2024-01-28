e824ad99580bb65cbdb156ee0802d91395b5215b
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-28 15:12:02 +0300

    feat: implement streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { stdin, stdout } from "node:process";
+import { Transform } from "node:stream";
+
+const reverseString = new Transform({
+    transform(data, encoding, callback) {
+        this.push(data.toString().trim().split("").reverse().join("") + "\n");
+        callback();
+    },
+});
+
 const transform = async () => {
-    // Write your code here 
+    stdin.pipe(reverseString).pipe(stdout);
 };
 
 await transform();
