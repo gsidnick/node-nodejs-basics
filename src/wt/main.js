525c15f6c7a3a1795faa8f624039dc3dc8e71b08
Nikolay Sidorenko <gsidnick@gmail.com>
2024-01-29 09:05:26 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import os from "node:os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Worker } from "node:worker_threads";
+
+const createWorker = (value) => {
+    return new Promise((resolve, reject) => {
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const workerFile = join(__dirname, "worker.js");
+        const worker = new Worker(workerFile, { workerData: { n: value } });
+        worker.on("message", (result) => {
+            resolve({
+                status: "resolved",
+                data: result,
+            });
+        });
+        worker.on("error", (error) => {
+            reject({
+                status: "error",
+                data: null,
+            });
+        });
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const promises = [];
+    const result = [];
+
+    os.cpus().forEach((cpu, index) => {
+        const n = 10 + index;
+        promises.push(createWorker(n));
+    });
+
+    const promisesResult = await Promise.allSettled(promises);
+    promisesResult.forEach((obj) => {
+        Object.entries(obj).forEach(([key, value]) => {
+            if (key.match(/value|reason/)) {
+                result.push(value);
+            }
+        });
+    });
+
+    console.log(result);
 };
 
 await performCalculations();
